0cc6ee6514f26aef98b807b36053e0ac7db9ad7d
LilianKalkman <liliankalkman@hotmail.com>
2018-05-18 23:43:27 +0200

    challenges 2

--- challenges.js
+++ challenges.js
@@ -1,20 +1,25 @@
+const scoreInput = document.querySelector('#input-score');
+const scoreOutput = document.querySelector('.final-score');
 
 var scores, roundScore, activePlayer, gamePlaying;
 
 var dicePrevious;
+var winningScore;
 
 init();
 
+scoreInput.addEventListener('change', function(event){
+  winningScore = event.target.value;
+  event.target.value = '';
+  this.style.display = 'none';
+  scoreOutput.textContent = winningScore;
+  scoreOutput.style.display = 'inline';
+})
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
         // 1. Random number
         var dice = Math.floor(Math.random() * 6) + 1;
-        dicePrevious = dice;
-
-        // console.log(dicePrevious);
-        // if 2 times 6 after each other; next player
-
 
         //2. Display the result
         var diceDOM = document.querySelector('.dice');
@@ -23,7 +28,12 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
         //3. Update the round score IF the rolled number was NOT a 1
-        if (dice !== 1) {
+        if(dice === 6 && dicePrevious === 6){
+          // looses scores and next player
+          scores[activePlayer] = 0;
+          document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+          nextPlayer();
+        } else if (dice !== 1) {
             //Add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
@@ -31,6 +41,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             //Next player
             nextPlayer();
         }
+
+        dicePrevious = dice;
     }
 });
 
@@ -44,7 +56,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         // Check if player won the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -83,6 +95,8 @@ function init() {
     activePlayer = 0;
     roundScore = 0;
     gamePlaying = true;
+    scoreOutput.style.display = 'none';
+    scoreInput.style.display = 'inline';
 
     document.querySelector('.dice').style.display = 'none';
 
@@ -102,3 +116,25 @@ function init() {
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 //var x = document.querySelector('#score-0').textContent;
+
+
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
